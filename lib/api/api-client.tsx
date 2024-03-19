abad7ca6389fd95712e0bbd5f110767c6be6a58c
BalajiMalathi <balaji@renacto.com>
2024-03-19 22:55:00 +0530

    API client update

--- lib/api/api-client.tsx
+++ lib/api/api-client.tsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { decryptToken, encryptToken } from './crypto';
-
 // Create an Axios instance
 const baseUrl = process.env.BASE_URL; // Use a secure key stored in env vars
 
@@ -14,45 +13,49 @@ const apiClient = axios.create({
 // Function to call to refresh tokens
 const refreshAuthToken = async (refreshToken: string) => {
     try {
-        const response = await apiClient.post('/refresh-token', { refresh_token: refreshToken });
+        const response = await apiClient.post('/auth/refresh-token', { refresh_token: refreshToken });
         return response.data;
     } catch (error) {
         console.error('Error refreshing auth token:', error);
         throw error; // Re-throw to catch this elsewhere
     }
 };
- 
 
-function parseJwt(token: string) {
+
+export async function parseJwt(token: string) {
     if (!token) { return; }
     const base64Url = token.split('.')[1];
     const base64 = base64Url.replace('-', '+').replace('_', '/');
     return JSON.parse(window.atob(base64));
 }
 
+export const getToken = () => localStorage.getItem("token")
+    ? localStorage.getItem("token")
+    : null;
+
+export const getAuthorizationHeader = () => `Bearer ${getToken()}`
+
 // Interceptor for request
 apiClient.interceptors.request.use(
     async (config) => {
-        console.log(config.baseURL);
-        console.log(config.data);
-        let token = localStorage.getItem('token');
+        let token = localStorage.getItem("token"); 
         if (token) {
-
             token = decryptToken(token);
-            const { exp } = parseJwt(token); // Decode token to get expiry time
-            const currentTime = Date.now() / 1000; // Convert to seconds
-
+            const { exp } = await parseJwt(token); // Decode token to get expiry time
+            const currentTime = Date.now() / 1000; // Convert to seconds 
+            // console.log(exp);
+            // console.log(currentTime);
             if (exp < currentTime) {
                 // Token is expired, refresh it
-                var refToken = localStorage.getItem('refresh_token');
+                var refToken = localStorage.getItem("refreshToken");
                 refToken = decryptToken(refToken!);
-                const {token, expiry, refreshToken} = await refreshAuthToken(refToken!);
-                 
+                const { token, expiry, refreshToken } = await refreshAuthToken(refToken!);
+
                 var encToken = encryptToken(token);
-                var encRef = encryptToken(token);
+                var encRef = encryptToken(refreshToken);
 
                 localStorage.setItem('token', encToken); // Update stored token
-                localStorage.setItem('refresh_token', encRef); // Update stored token 
+                localStorage.setItem('refreshToken', encRef); // Update stored token 
             }
 
             config.headers['Authorization'] = `Bearer ${token}`;
