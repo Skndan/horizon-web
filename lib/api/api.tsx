da65d83e2019db858f457602bf3653acbfdf9a7b
BalajiMalathi <balaji@renacto.com>
2024-07-20 17:07:32 +0530

    base url changed

--- lib/api/api.tsx
+++ lib/api/api.tsx
@@ -15,7 +15,7 @@ export function setupAPIClient(ctx?: GetServerSidePropsContext) {
   let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    baseURL: "https://api.horizon.skndan.com/api"
   });
 
   api.interceptors.response.use(
