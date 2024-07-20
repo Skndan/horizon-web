2b834e7eb69c0ba2b4c09fe1c0422e5a27cf6418
BalajiMalathi <balaji@renacto.com>
2024-07-21 00:24:34 +0530

    Status cell handling Employee status

--- components/common/status-cell.tsx
+++ components/common/status-cell.tsx
@@ -6,8 +6,8 @@ const StatusCell = ({ status }: { status: string }) => {
 
     var ripple = <></>;
 
-    /**
-     
+    /** 
+NOTICE  
     status == "INITIATED" ? <span className="relative flex h-3 w-3">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-200 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
@@ -17,13 +17,15 @@ const StatusCell = ({ status }: { status: string }) => {
         case "INITIATED":
         case "DRAFT":
         case "HOLD":
+        case "PROBATION":
             ripple = <span className="relative flex h-3 w-3">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-cyan-200 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-cyan-400"></span>
             </span>
-            break; 
+            break;
         case "APPROVED":
         case "HIRING":
+        case "ACTIVE":
             ripple = <span className="relative flex h-3 w-3">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-200 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
@@ -31,9 +33,23 @@ const StatusCell = ({ status }: { status: string }) => {
             break;
         case "REJECTED":
         case "CLOSED":
+        case "RESIGNED":
+        case "TERMINATED":
             ripple = <span className="relative flex h-3 w-3">
                 <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-200 opacity-75"></span>
                 <span className="relative inline-flex rounded-full h-3 w-3 bg-red-400"></span>
+            </span>
+            break;
+        case "DECEASED":
+            ripple = <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-stone-200 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-stone-400"></span>
+            </span>
+            break;
+        case "NOTICE":
+            ripple = <span className="relative flex h-3 w-3">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-amber-200 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-3 w-3 bg-amber-400"></span>
             </span>
             break;
     }
